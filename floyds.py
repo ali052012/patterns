f65fb4c3e17fc8b48bc118aef6f34d4dd7ba1b0a
ali052012 <aliwarisshah025@gmail.com>
2025-04-19 14:09:56 +0500

    floyds

---
+++ floyds.py
@@ -0,0 +1,15 @@
+#take input fro user
+rows=int(input("please enter the total number of rows:"))
+number=1 #initialise by 1
+
+
+print("floyd's triangle")
+#outer loop for numbers of rows 
+for i in range(1, rows+1):
+    #inner loop for number of column
+    for j in range (1,i+1):
+        #display result
+        print(number,end= ' ')
+        number+=1
+    print()
+          
